947eb0eddc3b2efaa634855e0e621933ffee8449
Git-Jake-Hill <89761486+Git-Jake-Hill@users.noreply.github.com>
2022-05-24 09:54:43 +1000

    Initial commit Resize Tasks

---
+++ ResizeImagesWithTasks/ResizeImagesWithTasks.py
@@ -0,0 +1,45 @@
+import sys
+import os
+import traceback
+
+from Deadline.Events import DeadlineEventListener
+
+
+def GetDeadlineEventListener():
+	return EventScriptListener()
+
+
+def CleanupDeadlineEventListener(eventListener):
+	eventListener.Cleanup()
+
+
+class EventScriptListener(DeadlineEventListener):
+
+	def __init__(self):
+		self.OnSlaveStartingJobCallback += self.OnSlaveStartingJob
+		self.OnJobStartedCallback += self.OnJobStarted
+		self.OnJobFinishedCallback += self.OnJobFinished
+		self.OnJobFailedCallback += self.OnJobFailed
+		self.OnJobSuspendedCallback += self.OnJobSuspended
+
+	def Cleanup(self):
+		del self.OnSlaveStartingJobCallback
+		del self.OnJobStartedCallback
+		del self.OnJobFinishedCallback
+		del self.OnJobFailedCallback
+		del self.OnJobSuspendedCallback
+
+	def OnSlaveStartingJob(self, slaveName, job):
+		self.LogInfo( "SLAVE STARTING JOB" )
+			
+	def OnJobStarted(self, job):
+		self.LogInfo( "JOB STARTED" )
+		
+	def OnJobFinished(self, job):
+		self.LogInfo( "JOB FINISHED" )		
+		
+	def OnJobFailed(self, job):
+		self.LogInfo( "JOB FAILED" )		
+		
+	def OnJobSuspended(self, job):
+		self.LogInfo( "JOB SUSPENDED" )		
