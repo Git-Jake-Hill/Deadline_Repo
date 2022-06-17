d97af364e3b851eb67693c9fed10b067a9b45de9
Jacob@Scharp <jacob@scharp.com.au>
2022-06-17 13:13:51 +1000

    Finished implementation for resize anim script

--- ResizeAnimations/Resize_anim_script.py
+++ ResizeAnimations/Resize_anim_script.py
@@ -1,112 +1,95 @@
 import Draft
+from DraftParamParser import ReplaceFilenameHashesWithNumber
 import sys 
 import re
-import os
 
-# load image size settings
-FinalFrameWidth = 4500
-FinalFrameHeight = 3000
-outFolder = ""
+def resize_frame(final_frame_width, final_frame_height, input_file, out_file=None):
+    """resize image frame.
+
+    Args:
+        FinalFrameWidth (int): _description_
+        FinalFrameHeight (int): _description_
+        InputFile (string): _description_
+        OutFile (string): _description_
+    """
+    try:
+    
+    # set output file location if none supplied
+        if out_file == None:
+            out_file = input_file
+        imageInfo = Draft.ImageInfo()
+
+    # open image
+        img = Draft.Image.ReadFromFile( input_file, imageInfo=imageInfo )
+        fileChannelMap = img.GetFileChannelMap()
+
+        if img.HasChannel( 'R' ):
+            print(' Image has Red channel')
+        else:
+            print(' Image does not have a channel')
+
+            # rename channels to only have RGB, by removing superfluous text
+            keys = fileChannelMap.keys()
+            for key_val in keys:
+                channel_name, channel_val = key_val.rsplit( '.', 1 )
+        
+            img.RenameChannel( channel_name + '.R', 'R' )
+            img.RenameChannel( channel_name + '.G', 'G' )
+            img.RenameChannel( channel_name + '.B', 'B' )
+
+                
+    # resize - pass in size from FinalFrameWidth, FinalFrameHeight extra info
+        img.Resize( final_frame_width, final_frame_height, 'height' )
+        print("Complete - resize image")
+
+    # copy image to correct image size
+        newImage = Draft.Image.CreateImage( final_frame_width, final_frame_height, channels=['R', 'G', 'B'] )
+        newImage.Copy( img, channels=['R', 'G', 'B'] )
+
+    # compression
+        newImageInfo = Draft.ImageInfo()
+        newImageInfo.compression = 'zips'
+
+    # save to path
+        newImage.WriteToFile( out_file, newImageInfo )
+
+    except:
+        print("ERROR - Failed to Resize:", input_file)
+
 
 print("-----Start [RESIZE ANIM] script job-----")
-# access extra info from parent job args
-print("***Arguments:", sys.argv)
 
+FRAME_LIST = []
+TASK_ID = None
+# access extra info from parent job args
 for item in sys.argv:
     print("  List args:", item)
 
+    # Task ID == Frame number
     if item.startswith("taskStartFrame="):
-        task_id = int(item.split("=")[-1]) -1
-        print("Task ID:", task_id)
+        TASK_ID = int(item.split("=")[-1]) 
+        print("Task ID:", TASK_ID)
 
-    if item.startswith("finalFrameWidth="):
-        FinalFrameWidth = int(item.split("=")[-1])
-        print("Image width:", FinalFrameWidth)
-        # print("could not find FinalImageWidth=")
+    elif item.startswith("FinalFrameWidth="):
+        final_frame_width = int(item.split("=")[-1])
+        print("Image width:", final_frame_width)
 
-    if item.startswith("finalFrameHeight="):
-        FinalFrameHeight = int(item.split("=")[-1])
-        print("Image height:", FinalFrameHeight)
+    elif item.startswith("FinalFrameHeight="):
+        final_frame_height = int(item.split("=")[-1])
+        print("Image height:", final_frame_height)
 
-    if item.startswith("outFolder="):
-        outFolder = item.split("=")[-1]
-        print("Current folder:", outFolder)
+    elif item.startswith("frame_output"):
+        currentFrame = item.split("=")[-1]
+        print("Current frame:", currentFrame)
+        FRAME_LIST.append(currentFrame)
 
-# set inFolder to outFolder of parent deadline job
-inFolder = outFolder + '\\'
-print(" ***In folder:", inFolder)
+for frame in FRAME_LIST:
+        # search for _RE. for render elemnts only
+        tile_regex = re.compile("_RE.") 
+        if tile_regex.search(frame) != None:
+            input_file = ReplaceFilenameHashesWithNumber( frame , TASK_ID )
+            print("Input frame:", input_file)
+            resize_frame(final_frame_width, final_frame_height, input_file)
 
-# set outFolder to be parent directory of inFolder
-outFolder = outFolder.rsplit('\\', 1)[0] + '\\'
-print(" ***Out folder:", outFolder)
-
-
-# Get files to create one task per image
-print("GET FILES:")
-print(" ")
-
-# files in the inFolder directory
-# files = os.listdir( inFolder ) 
-# print(str(files))
-
-# search for _tile_ in the output directory
-tile_regex = re.compile("_tile_") 
-
-# print("LIST OF FILES:")
-# exr_list = []
-# for file in files:
-#     if file.endswith(".exr"):
-#     # if file.startswith("V01_RE"): # test on only render elements
-#         # ignore files and folder that are not exr
-#         if tile_regex.search(file) == None:
-#             # must not be a tile
-#             print(f"Files: {file}")
-#             exr_list.append(str(file))
-
-file = "A01_Render - BF_.0003.exr"
-inFolder = "R:/Project/SCH001 Scharp R&D/Ben H/Post Image Submission/Rig Test A01/Render - BF/test/"
-outFolder = "R:/Project/SCH001 Scharp R&D/Ben H/Post Image Submission/Rig Test A01/Render - BF/test/out/"
-print("\nResize:", file)
-
-try:
-    img_path = inFolder + file # set the input file (VIEW/TILES subfolder)
-    img_out_path = outFolder + file # set the output file (VIEW folder)
-    imageInfo = Draft.ImageInfo()
-
-    # open image
-    img = Draft.Image.ReadFromFile( img_path, imageInfo=imageInfo )
-    fileChannelMap = img.GetFileChannelMap()
-
-    if img.HasChannel( 'R' ):
-        print(' Image has Red channel')
-    else:
-        print(' Image does not have a channel')
-
-        keys = fileChannelMap.keys()
-        for key_val in keys:
-            channel_name, channel_val = key_val.rsplit( '.', 1 )
-        
-        img.RenameChannel( channel_name + '.R', 'R' )
-        img.RenameChannel( channel_name + '.G', 'G' )
-        img.RenameChannel( channel_name + '.B', 'B' )
-
-                
-    # resize - pass in size from extra info in [exrtract] pass
-    img.Resize( FinalFrameWidth, FinalFrameHeight, 'height' )
-    print("Complete - resize image")
-
-    # copy image to correct image size
-    newImage = Draft.Image.CreateImage( FinalFrameWidth, FinalFrameHeight, channels=['R', 'G', 'B'] )
-    newImage.Copy( img, channels=['R', 'G', 'B'] )
-
-    # compression
-    newImageInfo = Draft.ImageInfo()
-    newImageInfo.compression = 'zips'
-
-    # save to path
-    newImage.WriteToFile( img_out_path, newImageInfo )
-
-except:
-    print("ERROR - Failed to Resize:", file)
 
 
